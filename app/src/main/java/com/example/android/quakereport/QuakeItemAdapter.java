13a43ebaee86a05cf940d005feec49fc5c5f2d69
Apurba Biswas <apurba10024@gmail.com>
2018-02-17 01:58:45 +0600

    1.8 Added color for different magnitudes

--- app/src/main/java/com/example/android/quakereport/QuakeItemAdapter.java
+++ app/src/main/java/com/example/android/quakereport/QuakeItemAdapter.java
@@ -7,11 +7,13 @@ package com.example.android.quakereport;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.graphics.drawable.GradientDrawable;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -44,8 +46,18 @@ public class QuakeItemAdapter extends ArrayAdapter<QuakeItem>{
      *
      */
     private void setAllViews(QuakeItem currentEarthquake, View listQuakeItemView){
+
+
         //find and Set the magnitude
-        setATextView((TextView) listQuakeItemView.findViewById(R.id.magnitude_text_field), formatedMagnitude(currentEarthquake.getMagnitude()));
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        TextView magnitudeView = listQuakeItemView.findViewById(R.id.magnitude_text_field);
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+        setATextView(magnitudeView, formatedMagnitude(currentEarthquake.getMagnitude()));
 
         //find and set the location offset
         currentEarthquake.splitLocation();
@@ -61,6 +73,46 @@ public class QuakeItemAdapter extends ArrayAdapter<QuakeItem>{
         //create human readable formate for time and then display it
         setATextView((TextView) listQuakeItemView.findViewById(R.id.time), formatTime(dateObject));
     }
+
+    private int getMagnitudeColor(double magnitude){
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor){
+            case 0 :
+            case 1 :
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2 :
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3 :
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4 :
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5 :
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6 :
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7 :
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8 :
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9 :
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
+    }
+
     /**
      * Set the Specified Text view with a given text
      */
